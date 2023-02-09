2c01fd9a86279b607de74daee1f8263dae7dc6e8
awsanto <alexandrewinkel@gmail.com>
2023-02-09 17:00:17 -0300

    deploy

--- api/main.py
+++ api/main.py
@@ -48,4 +48,4 @@ async def download_combustivel(params: Params):
         raise HTTPException(status=ex.code, detail=f"{ex}")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port = 8080)
+    uvicorn.run(app, host="0.0.0.0", port = 8080)
